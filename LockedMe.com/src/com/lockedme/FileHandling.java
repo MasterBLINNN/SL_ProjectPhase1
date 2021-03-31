f9f62b3c24f11508c53f59291b5cea3c3e831f20
MasterBLINNN <lukaszblicharz@gmail.com>
2021-03-31 12:59:04 -0300

    update

--- LockedMe.com/src/com/lockedme/FileHandling.java
+++ LockedMe.com/src/com/lockedme/FileHandling.java
@@ -2,17 +2,14 @@ package com.lockedme;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.DirectoryNotEmptyException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
 public class FileHandling {
 
-
+	private static final String path = "Files\\";
+	
 	
 	public void fileCheckOnDirectory()
     {
@@ -23,6 +20,7 @@ public class FileHandling {
             File[] fileList = folder.listFiles();
 
             Arrays.sort(fileList);
+           
 
             System.out.println("\nDirectory: " + folder.getAbsolutePath());
             
@@ -47,9 +45,11 @@ public class FileHandling {
         }
         }
 	
+	
+
 	public void createFile(String name) throws IOException {
 		
-		File file = new File("Files\\"+name);
+		File file = new File(path(name));
 		if (file.createNewFile()) {
 		System.out.println("File is created.");	
 		} else { System.out.println("File already exists.");
@@ -58,19 +58,62 @@ public class FileHandling {
 		}
 		
 	
-	public void deleteFile(String name) {
+	public void deleteFile(String name) throws IOException {
 		
-	    	 File file = new File("Files\\"+name);
+	    	 File file = new File(path(name));
+	    	    	 
+	    if(fileExistsCaseSensitive(path(name))) {
+	  
+	    	file.delete();
+	    	System.out.println("Deleted the file: " + file.getName());
+			    } 
+	    	else {
+			System.out.println("Failed to delete the file. File doesn't not exist");
+			    }   
+	    	 
+	     }
+	
+
+	
+	public void searchFile(String name) {    
+			
+			
+				
+	    	 File file = new File(path(name));
 	    
-	    	 if (file.delete()) { 
-			      System.out.println("Deleted the file: " + file.getName());
+	    	 if(fileExistsCaseSensitive(path(name)))  { 
+			      System.out.println("File : " + file.getName()  + " exist in the directory");
 			    } else {
-			      System.out.println("Failed to delete the file. File doesn't not exist");
+			      System.out.println("File does not exist.");
 			    } 
 	    	 
 	     }
+
+	
+	public static boolean fileExistsCaseSensitive(String path) {
+	    try {
+	        File file = new File(path);
+	        return file.exists() && file.getCanonicalFile().getName().equals(file.getName());
+	    } catch (IOException e) {
+	        return false;
+	    }
+	}
+	
+
+	public static String getPath() {
+		return path;
+	}
+	
+	public static String path(String name) {
+		String path = getPath()+name;
+		
+		return path;
+	}
+	
+	
+        }
 		
 	
-	}
+	
 	
 	
